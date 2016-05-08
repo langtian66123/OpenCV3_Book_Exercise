ba505697d947d7908ddef3e725db062ec5d283e8
guojia <947081171@qq.com>
2016-05-08 16:00:58 +0800

    modify 增加判断条件，因为摄像头打开没有读取到一帧，所以抛出异常，需要该语句判断

--- OpenCV3_Exercise/7_UseCamera/7_UseCamera.cpp
+++ OpenCV3_Exercise/7_UseCamera/7_UseCamera.cpp
@@ -3,19 +3,27 @@ using namespace cv;
 
 int main(){
 	VideoCapture capture(0); //参数为0，即获取摄像头
-	Mat frame,edges;
+	Mat edges;
 	
 	while(1){
-		capture>>frame;
+		Mat frame;
+		capture >> frame;
 
 		//边缘检测
-		cvtColor(frame,edges,CV_BayerRG2GRAY);
-		blur(edges,edges,Size(7,7));
-		Canny(edges,edges,0,30,3);
+		if(!frame.empty()){ //注意一定要加上if判断，不然会抛出异常
+			cvtColor(frame,edges,CV_BGR2GRAY);
+			blur(edges,edges,Size(7,7));
+			Canny(edges,edges,0,30,3);
+			imshow("读取视频",edges);
+			if(waitKey(30)>=0)
+				break;
+		}
 
-		imshow("读取视频",edges);
-		if(waitKey(30)>=0)
-			break;
+		//if(!frame.empty()){
+		//	imshow("读取视频",frame);
+		//}
+
+		//waitKey(30);
 	}
 	return 0;
 }
